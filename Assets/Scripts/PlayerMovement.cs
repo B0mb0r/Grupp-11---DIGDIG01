47afdd77596951320559187778e968ccfb1a318e
Johnny.Chu <johnny.chu@elev.ga.lbs.se>
2019-01-29 08:35:58 +0100

    FlyingEnemy2

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [Range(0, 20f)]
+    public float moveSpeed;
+    public float jumpHeight;
+
+    public GroundCheck groundCheck;
+
+    private Rigidbody2D rbody;
+
+    // Use this for initialization
+    void Start()
+    {
+        // gör att rbody blir ditt egna Rigidbody
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // unitys inbyggda movement script
+        rbody.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), rbody.velocity.y);
+        // om man trycker på w och groundchek rör en collider så hoppar man
+        if (Input.GetKeyDown(KeyCode.W) && groundCheck.touches > 0)
+        {
+            rbody.velocity = new Vector2(rbody.velocity.x, jumpHeight);
+        }
+        // om man trycker på space och groundcjek rör en collider så hoppar man
+        if (Input.GetKeyDown(KeyCode.Space) && groundCheck.touches > 0)
+        {
+            rbody.velocity = new Vector2(rbody.velocity.x, jumpHeight);
+        }
+    }
+
+}
