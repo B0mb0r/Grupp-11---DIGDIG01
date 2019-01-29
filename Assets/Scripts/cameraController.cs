f769b47367f629fb399fb2f3521a5d52dd713013
LEARNET\william.jutbring <william.jutbring@elev.ga.lbs.se>
2019-01-29 10:49:24 +0100

    cameralole

---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+
+    public BoxCollider2D boundBox;
+    private Vector3 minBounds;
+    private Vector3 maxBounds;
+    public GameObject player;
+
+    private Camera theCamera;
+    private float halfHeight;
+    private float halfWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        minBounds = boundBox.bounds.min;
+        maxBounds = boundBox.bounds.max;
+        theCamera = GetComponent<Camera>();
+        halfHeight = theCamera.orthographicSize;
+        halfWidth = halfHeight * Screen.width / Screen.height;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3( player.transform.position.x, player.transform.position.y, transform.position.z);
+        float clampedX = Mathf.Clamp(transform.position.x, minBounds.x + halfWidth, maxBounds.x - halfWidth);
+        float clampedY = Mathf.Clamp(transform.position.y, minBounds.y + halfHeight, maxBounds.y - halfHeight);
+        transform.position = new Vector3(clampedX, clampedY, transform.position.z);
+    }
+}
