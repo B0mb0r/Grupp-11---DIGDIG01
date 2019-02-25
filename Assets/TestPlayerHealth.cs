0871fe3ce4bd7ec36bdf631f4194debe5c269029
Simon Schachinger <simon.schachinger@elev.ga.lbs.se>
2019-02-25 08:58:08 +0100

    Pause and main menu

--- Assets/TestPlayerHealth.cs
+++ Assets/TestPlayerHealth.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class TestPlayerHealth : MonoBehaviour
 {
-    public int health;
+    public float health;
+    public float moveSpeed;
+
+    private Rigidbody2D rbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, rbody.velocity.y);
     }
 }
