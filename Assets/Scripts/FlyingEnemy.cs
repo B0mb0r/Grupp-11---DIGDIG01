22e8c5ba4be4f3d606273be98e0c03ece1dd4f98
Johnny.Chu <johnny.chu@elev.ga.lbs.se>
2019-04-09 09:14:24 +0200

    Reworkad grounded enemy
    gjorde att grounded enemy använder ground detection istället för invisible wall

---
+++ Assets/Scripts/FlyingEnemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemy : MonoBehaviour
+{
+    public float speed;
+    public float stopChasing;
+    public Transform target;
+    
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+    void Update()
+    {
+        if(Vector2.Distance(transform.position, target.position) < stopChasing)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+         
+    }
+}
