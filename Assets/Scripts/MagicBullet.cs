1d43020edcac2579c956a0f5e8e739ad1f07688d
HugoFagerlind <hugo.fagerlind@elev.ga.lbs.se>
2019-04-21 18:15:21 +0200

    ranged combat

---
+++ Assets/Scripts/MagicBullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicBullet : MonoBehaviour
+{
+
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    public int MBDamage = 1;
+
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        EnemyHealthSystem enemy = hitInfo.GetComponent<EnemyHealthSystem>();
+        if(enemy != null)
+        {
+            enemy.TakeDamage(MBDamage);
+        }
+        if (hitInfo.tag == "Enemy" || hitInfo.tag == "Ground")
+        {
+            Debug.Log("TJENA");
+            Destroy(gameObject);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/PlayerRangedAttack.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRangedAttack : MonoBehaviour
+{
+
+    public Transform FirePoint;
+    public GameObject MagicBullet;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire2"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(MagicBullet, FirePoint.position, FirePoint.rotation);
+    }
+}
