c18a985c67db39215c75d35e0403c76e6351a0f9
hugo fagerlind <hugo.fagerlind@elev.ga.lbs.se>
2019-04-23 08:50:34 +0200

    no message

--- Assets/Scripts/DamagePlayer.cs
+++ Assets/Scripts/DamagePlayer.cs
@@ -4,20 +4,15 @@ using UnityEngine;
 
 public class DamagePlayer : MonoBehaviour
 {
-<<<<<<< HEAD
     public Health health;
-=======
-    public Health Health;
->>>>>>> 7a8011e7027bc29b9fb825c233fffdeda3fbbcc5
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-<<<<<<< HEAD
-           health.health = health.health - 1;
-=======
-            Health.health = Health.health - 1;
->>>>>>> 7a8011e7027bc29b9fb825c233fffdeda3fbbcc5
+
+            health.health = health.health - 1;
+
         }
     }
     // Start is called before the first frame update
